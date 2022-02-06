ca0d9773c46917d739fdcc5c838eae49c3c659ce
Matthis HOLLEVILLE <matthish29@gmail.com>
2022-02-06 16:55:10 +0100

    feat: add base api client and maps controller

---
+++ pkg/haproxy/utils.go
@@ -0,0 +1,8 @@
+package haproxy
+
+import "encoding/base64"
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
